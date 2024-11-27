f6608590a50789c205c4f366f7fc58b765e4122d
PieStw <piero.stalteri@icloud.com>
2024-11-27 14:15:47 +0100

    creazione pagina

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -5,6 +5,30 @@ import "./App.css";
 import { tasks } from "./data/task";
 
 function App() {
-  
+  const [taskList, setTask] = useState(tasks);
+
+  const completedTask = taskList.filter((task) => task.state == "completed");
+  const uncompletedTask = taskList.filter((task) => task.state != "completed");
+
+  return (
+    <>
+      <h1>Current Tasks ({uncompletedTask.length})</h1>
+      {uncompletedTask.map((task, index) => (
+        <div key={index}>
+          <p>{task.title}</p>
+        </div>
+      ))}
+
+      <hr></hr>
+
+      <h1>Completed Tasks ({completedTask.length})</h1>
+      {completedTask.map((task, index) => (
+        <div key={index}>
+          <p>{task.title}</p>
+        </div>
+      ))}
+    </>
+  );
+}
 
 export default App;
